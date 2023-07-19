33639def86687f6b13b72f3824b3dfaf0ae8b3e7
bpavlov-1 <60926033+bpavlov-1@users.noreply.github.com>
2023-07-19 09:53:51 +0300

    Add jupyterhub login url (#2)

--- ckanext/keycloak_auth/views/keycloak_auth.py
+++ ckanext/keycloak_auth/views/keycloak_auth.py
@@ -5,6 +5,7 @@ import secrets
 
 from flask import Blueprint
 from six import text_type
+from six.moves.urllib.parse import urlparse
 
 import ckan.plugins.toolkit as toolkit
 import ckan.model as model
@@ -169,7 +170,24 @@ def keycloak_change_password():
 
     return toolkit.redirect_to(redirect_url)
 
+def jupyterhub_login():
+    u'''Redirects the user to jupyterhub
+    '''
+    ckan_url = config.get('ckanext.keycloak.ckan_url')
+    parsed_url = urlparse(ckan_url)
+    host = parsed_url.netloc.split(':')[0]
+    jupyterhub_host = 'jupyterhub.' + host
+
+    redirect_url = (
+        "https://"
+        + jupyterhub_host
+        + "/"
+    )
+
+    return toolkit.redirect_to(redirect_url)
+
 auth_endpoint = config.get('ckanext.keycloak_auth.auth_endpoint', '/authenticate')
 keycloak_auth.add_url_rule(auth_endpoint, view_func=auth, methods=[u'GET', u'POST'])
 keycloak_auth.add_url_rule(u'/user/keycloak_login', view_func=keycloak_login)
+keycloak_auth.add_url_rule(u'/user/jupyterhub_login', view_func=jupyterhub_login)
 keycloak_auth.add_url_rule(u'/user/keycloak_change_password', view_func=keycloak_change_password)
